58b1504f0c606215e800d4adbce4694d89330fa5
Gurmancheema <gurmanjotsingh97@gmail.com>
2026-03-30 10:03:58 +0530

    improved UI/UX design

--- app/main.py
+++ app/main.py
@@ -5,13 +5,42 @@ import streamlit as st
 print(st.__version__)
 
 # hard-coding front end to get basic user inputs required to Start a workout session
-st.title("Workout Tracker")
+st.set_page_config(
+    page_title="Workout Tracker",
+    page_icon="💪",
+    layout="wide"
+    )
+left, center, right = st.columns([1, 3, 1])
 
-user_name = st.text_input("Enter the Username")
+with center:
+    st.markdown("## 💪 Workout Tracker")
+    st.caption("Track your workouts efficiently")
 
-workout_date_input = st.date_input("Enter the workout date")
+    st.markdown("### 🏁 Start Workout")
 
-duration_input = st.number_input("Enter the expected workout duration in minutes")
+# for columnar format on page
+# let's fetch values from user on same page without letting the user to scroll down
+
+#defining column ratios
+    col1, col2, col3 = st.columns([2, 2, 1.5])
+
+# creating column labels
+    col1.markdown("**User**")
+    col2.markdown("**Date**")
+    col3.markdown("**Duration**")
+#col4.markdown("**Duration (min)**")
+#col5.markdown("**Action**")
+
+# defining column placeholders
+
+    with col1:
+        user_name = st.text_input("Username",key="user_name",label_visibility="collapsed")
+
+    with col2:
+        workout_date_input = st.date_input("Workout Date",key="workout_date",label_visibility="collapsed")
+
+    with col3:
+        duration_input = st.number_input("Workout Duration",min_value=10,step=1,key="workout_duration",label_visibility="collapsed")
 
 # creating a session state to hold the values of some variables
 # that are required throughout the environment
@@ -33,23 +62,25 @@ user_id_input = get_user_id(user_name)
 
 from db import create_workout_session
 
-if st.button("Start Workout"):
-    workout_session_id = create_workout_session(user_id_input,workout_date_input,duration_input)
-    st.success(f"Workout Session Created,{workout_session_id}")
-    st.session_state["workout_session_id"] = workout_session_id
+with center:
+    if st.button("Start Workout"):
+        workout_session_id = create_workout_session(user_id_input,workout_date_input,duration_input)
+        st.success(f"Workout Session Created,{workout_session_id}")
+        st.session_state["workout_session_id"] = workout_session_id
+
+    st.divider()
 
 
 # At the press of "Start Workout" button, the workout session details will be inserted into DB
 # and a unique workout_session_id will be created for that particular "user" for that particular "date"
 
 
+    st.markdown("### 🏋️ Select Exercise")
+
 # let's list some exercises for the user to perform in the created workout session
 # therefore, importing the function from DB that fetches all exercises from "exercises" table
 
 from db import get_all_exercises
-
-st.subheader("Add Exercises")
-
 exercises_dict ={}
 
 exercises = get_all_exercises()
@@ -64,81 +95,122 @@ for exercise in exercises:
 # creating exercise dropdown UI
 
 options  = [None] + list(exercises_dict.keys())
-selected_exercise = st.selectbox("Select Exercise",options, format_func= lambda x: "Select an exercise" if x is None else x)
+with center:
+    col7 = st.columns([2])
+
+    with col7[0]:
+        selected_exercise = st.selectbox("Select Exercise",options, format_func= lambda x: "Select an exercise" if x is None else x
+                                    ,label_visibility="collapsed")
 
 
 
-from db import create_workout_exercises
-if st.button("Add Exercise"):
-    workout_session_id = st.session_state["workout_session_id"]
-    if workout_session_id is None:
-        st.warning("Please add workout session details")
-    else:
-        # adding a warning
-        if selected_exercise is None:
-            st.warning("Please select an exercise")
-            st.stop()
+
+
+with center:
+    if st.button("Add Exercise"):
+        workout_session_id = st.session_state["workout_session_id"]
+        if workout_session_id is None:
+            st.warning("Please add workout session details")
         else:
-            exercise_id = exercises_dict[selected_exercise]
+            # adding a warning
+            if selected_exercise is None:
+                st.warning("Please select an exercise")
+                st.stop()
+            else:
+                exercise_id = exercises_dict[selected_exercise]
 
 
-    
+        
 
-        # fetch the current exercise order for the current workout session
-        from db import get_exercises_order
-        exercise_order = get_exercises_order(workout_session_id)
+            # fetch the current exercise order for the current workout session
+            from db import get_exercises_order
+            exercise_order = get_exercises_order(workout_session_id)
 
-        workout_exercises_id = create_workout_exercises(
-            workout_session_id,
-            exercise_id,
-            exercise_order
-        )
-        st.session_state["set_number"] = 1
-        st.success(f"Exercise added! ID: {workout_exercises_id}")
-        st.session_state["workout_exercises_id"] = workout_exercises_id
+            from db import create_workout_exercises
+            workout_exercises_id = create_workout_exercises(
+                workout_session_id,
+                exercise_id,
+                exercise_order
+            )
+            st.session_state["set_number"] = 1
+            st.success(f"Exercise added! ID: {workout_exercises_id}")
+            st.session_state["workout_exercises_id"] = workout_exercises_id
 
 # UI to insert the sets, reps & weights into the table "exercises_sets"
 
 from db import create_exercises_sets
 
-st.subheader("Add Sets")
+with center:
+    st.divider()
+    st.markdown("### 🏋️ Add Sets")
 
-set_number = st.session_state["set_number"]
-st.write(f"Set Number: {st.session_state['set_number']}")
-reps = st.number_input("Reps", min_value=1, step=1)
-weight = st.number_input("Weight (kg)", min_value=0.0, step=2.5)
-duration_seconds = st.number_input("Duration (secs)", min_value = 5, step = 1)
+    col8,col4, col5, col6 = st.columns([1,1, 1, 1])
+    col8.markdown("**Set Number**")
+    col4.markdown("**Reps**")
+    col5.markdown("**Weight**")
+    col6.markdown("**Duration**")
+    #col4.markdown("**Action**")
 
-# Initialize message state
-message_placeholder =st.empty()
+    set_number = st.session_state["set_number"]
+    with col8:
+        st.write(f"{st.session_state['set_number']}")
 
-if "success_message" not in st.session_state:
-    st.session_state["success_message"] = None
+    # handling the reset for user inputs before the widgets are created
+    # streamlit behaviour => Update session_state → THEN rerun → THEN UI reflects change
 
-if st.session_state["success_message"]:
-    message_placeholder.success(st.session_state["success_message"])
-    st.session_state["success_message"] = None
-    
-if st.button("Add Set"):
-    workout_exercises_id = st.session_state.get("workout_exercises_id")
+    if "reset_inputs" in st.session_state and st.session_state["reset_inputs"]:
+        st.session_state["reps"] = 1
+        st.session_state["duration"] = 5
+        st.session_state["reset_inputs"] = False
+    with col4:
+        reps = st.number_input("Reps", min_value=1, step=1, key= 'reps',label_visibility="collapsed")
 
-    if workout_exercises_id is None:
-        st.error("Please add an exercise first")
-    else:
-        set_id = create_exercises_sets(
-            workout_exercises_id,
-            set_number,
-            reps,
-            weight,
-            duration_seconds
-        )
+    with col5:
+        weight = st.number_input("Weight (kg)", min_value=0.0, step=2.5,key='weight',label_visibility="collapsed")
 
-        st.session_state["success_message"] = f"Set {st.session_state['set_number']} added!"
-        st.session_state["set_number"] += 1
-        st.rerun()
+    with col6:
+        duration_seconds = st.number_input("Duration (secs)", min_value = 5, step = 1,key= 'duration',label_visibility="collapsed")
 
-st.subheader("Workout Progress")
 
+    # Initialize message state
+    message_placeholder =st.empty()
+
+    if "success_message" not in st.session_state:
+        st.session_state["success_message"] = None
+
+    if st.session_state["success_message"]:
+        message_placeholder.success(st.session_state["success_message"])
+        st.session_state["success_message"] = None
+        
+    if st.button("Add Set"):
+        workout_exercises_id = st.session_state.get("workout_exercises_id")
+
+        if workout_exercises_id is None:
+            st.error("Please add an exercise first")
+        else:
+            set_id = create_exercises_sets(
+                workout_exercises_id,
+                set_number,
+                reps,
+                weight,
+                duration_seconds
+            )
+
+            # display the success message for added set
+            st.session_state["success_message"] = f"Set {st.session_state['set_number']} added!"
+
+            # increment the set number
+            st.session_state["set_number"] += 1
+
+            # reset the inputs flag trigger
+            st.session_state["reset_inputs"] = True
+
+            st.rerun()
+st.divider()
+
+
+
+st.markdown("###                 📊 Workout Progress")
 from db import get_whole_workout_session
 
 from collections import defaultdict
