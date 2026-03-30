34587e0696a681926382ee152560cdeea5c2c6b5
Gurmancheema <gurmanjotsingh97@gmail.com>
2026-03-30 08:45:12 +0530

    feat: add auto-increment sets, user mapping, duplicate prevention, and live workout view

--- app/db.py
+++ app/db.py
@@ -1,6 +1,9 @@
-# creating a database connection
+# Importing necessary packages
+# Psycopg2-binary is necessary package to enable communication between Python & Postgresql
 import psycopg2
 
+# creating a function to establish database connection
+# need only 4 arguments
 def get_connection():
     return psycopg2.connect(
         host = 'localhost',
@@ -9,9 +12,25 @@ def get_connection():
         password = 'password'
 )
 
-# creating a function that gets user_id, workout_date and workout_duration
-# inserts the values into the workout_sessions table
-# then returns the workout_id & further closes the DB connection to avoid leaks
+# Creating a function that fetches the "user_id" from "user_name" entered by the user 
+def get_user_id (user_name):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute (""" SELECT user_id FROM workout.users WHERE LOWER(name) = LOWER (%s);""",
+                  (user_name,))
+    
+    user_id = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return user_id
+
+
+# creating a function that fetches the "user_id", "workout_date" and "workout_duration" from user input
+# inserts theese values into the "workout_sessions" table
+# then returns the "workout_session_id" & further closes the DB connection to avoid leaks
 def create_workout_session(user_id,workout_date,duration_minutes):
     conn = get_connection()
     cur  = conn.cursor()
@@ -30,8 +49,7 @@ def create_workout_session(user_id,workout_date,duration_minutes):
 
     return workout_session_id
 
-# creating a function to fetch all exercises 
-
+# creating a function to fetch all the exercises listed in "exercises" table 
 def get_all_exercises():
     conn = get_connection()
     cur = conn.cursor()
@@ -45,13 +63,14 @@ def get_all_exercises():
     
     return exercises
 
-# creating another function to insert the values into workout_exercises table
+# creating another function to insert the values into "workout_exercises" table
+# this function returns the "workout_exerices_id"
+# this table is junction/bridge table which connects three facts tables "workout_sessions","exercises" & "exercises_sets"
 
 def create_workout_exercises(workout_session_id,exercise_id,exercise_order):
     conn = get_connection()
     cur = conn.cursor()
 
-    # what this function does is insert the values into workout_exercises table
     cur.execute(""" INSERT INTO workout.workout_exercises (workout_session_id,exercise_id,exercise_order) VALUES 
                 (%s,%s,%s) RETURNING workout_exercises_id; """,(workout_session_id,exercise_id,exercise_order))
     
@@ -63,10 +82,8 @@ def create_workout_exercises(workout_session_id,exercise_id,exercise_order):
 
     return workout_exercises_id
 
-# finally creating a function insert the sets,reps & weights user performed
-# in a single workout session , fetching the performed exercises and workout session
-# from workout_exercises table
-
+# finally creating a function that fetches the "sets","reps" & "weights" user performed
+# in a single workout session
 def create_exercises_sets(workout_exercises_id,set_number,reps,weight,duration_seconds):
     conn = get_connection()
     cur = conn.cursor()
@@ -82,21 +99,44 @@ def create_exercises_sets(workout_exercises_id,set_number,reps,weight,duration_s
 
     return set_id
 
-def get_exercises_for_workout(workout_session_id):
+# since the order of exercises performed must be incremented automatically each time user completes one exercise
+# therefore, creating a function that fetches the latest count of "exercise_order" in "workout_exercises" table
+# and increments it whenever the user adds a new exercise
+def get_exercises_order(workout_session_id):
     conn = get_connection()
     cur = conn.cursor()
 
-    cur.execute("""
-        SELECT we.workout_exercises_id, e.exercise_name
-        FROM workout.workout_exercises we
-        JOIN workout.exercises e
-        ON we.exercise_id = e.exercise_id
-        WHERE we.workout_session_id = %s;
-    """, (workout_session_id,))
-
-    data = cur.fetchall()
-
+    cur.execute(""" SELECT COALESCE (MAX(exercise_order),0) FROM workout.workout_exercises
+                WHERE workout_session_id = %s;""", (workout_session_id,))
+    
+    resulting_exercise_order = cur.fetchone()[0]
     cur.close()
     conn.close()
 
-    return data
+    return resulting_exercise_order + 1
+
+# To track the progress of a workout session, creating a function that fetches the following from DB:
+# 1. exercise_id, exercise_name, muscle_group from "exercises" table
+# 2. set_number, reps, weight from "exercises_sets" table
+# 3. since "workout_exercises" table is a junction table therefore, joining these 3 tables to get the analytics
+
+def get_whole_workout_session(workout_session_id):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""SELECT e.exercise_id,e.exercise_name,e.muscle_group,
+                we.exercise_order,
+                es.set_number,
+                es.reps,
+                es.weight FROM workout.workout_exercises we
+                JOIN workout.exercises_sets es
+                ON we.workout_exercises_id = es.workout_exercises_id
+                JOIN workout.exercises e
+                ON we.exercise_id = e.exercise_id 
+                WHERE we.workout_session_id = %s;""",(workout_session_id,))
+    
+    resulting_rows = cur.fetchall()
+    conn.close()
+    cur.close()
+
+    return resulting_rows
--- app/main.py
+++ app/main.py
@@ -1,22 +1,13 @@
-print("helloworld")
-import sys
-print(sys.version)
-
 # working on front-end now , since i decided to use streamlit therefore
 # creating an instance & importing from it
 
 import streamlit as st
-
 print(st.__version__)
 
-# importing the database connection files
+# hard-coding front end to get basic user inputs required to Start a workout session
+st.title("Workout Tracker")
 
-from db import create_workout_session
-
-# hard-coding front end
-st.title("Workout_tracker")
-
-user_id_input = st.text_input("Enter the user ID")
+user_name = st.text_input("Enter the Username")
 
 workout_date_input = st.date_input("Enter the workout date")
 
@@ -31,14 +22,33 @@ if "workout_session_id" not in st.session_state:
 if "set_number" not in st.session_state:
     st.session_state["set_number"] = 1
 
+
+# fetching user_id from the entered username from  the "users" table
+from db import get_user_id
+
+user_id_input = get_user_id(user_name)
+
+# since now we have our user fetched from DB, let's fetch the function from DB that will store
+# workout session details
+
+from db import create_workout_session
+
 if st.button("Start Workout"):
     workout_session_id = create_workout_session(user_id_input,workout_date_input,duration_input)
     st.success(f"Workout Session Created,{workout_session_id}")
     st.session_state["workout_session_id"] = workout_session_id
 
-# creating exercise dropdown UI
+
+# At the press of "Start Workout" button, the workout session details will be inserted into DB
+# and a unique workout_session_id will be created for that particular "user" for that particular "date"
+
+
+# let's list some exercises for the user to perform in the created workout session
+# therefore, importing the function from DB that fetches all exercises from "exercises" table
 
 from db import get_all_exercises
+
+st.subheader("Add Exercises")
 
 exercises_dict ={}
 
@@ -50,21 +60,42 @@ for exercise in exercises:
     display_name = name + " (" + muscle_group + ")"
     exercises_dict[display_name] = exercise_id
 
-selected_exercise = st.selectbox("Select Exercise", list(exercises_dict.keys()))
+
+# creating exercise dropdown UI
+
+options  = [None] + list(exercises_dict.keys())
+selected_exercise = st.selectbox("Select Exercise",options, format_func= lambda x: "Select an exercise" if x is None else x)
+
+
 
 from db import create_workout_exercises
 if st.button("Add Exercise"):
     workout_session_id = st.session_state["workout_session_id"]
-    exercise_id = exercises_dict[selected_exercise]
+    if workout_session_id is None:
+        st.warning("Please add workout session details")
+    else:
+        # adding a warning
+        if selected_exercise is None:
+            st.warning("Please select an exercise")
+            st.stop()
+        else:
+            exercise_id = exercises_dict[selected_exercise]
 
-    workout_exercises_id = create_workout_exercises(
-        workout_session_id,
-        exercise_id,
-        exercise_order=1  # we’ll improve this next
-    )
-    st.session_state["set_number"] = 1
-    st.success(f"Exercise added! ID: {workout_exercises_id}")
-    st.session_state["workout_exercises_id"] = workout_exercises_id
+
+    
+
+        # fetch the current exercise order for the current workout session
+        from db import get_exercises_order
+        exercise_order = get_exercises_order(workout_session_id)
+
+        workout_exercises_id = create_workout_exercises(
+            workout_session_id,
+            exercise_id,
+            exercise_order
+        )
+        st.session_state["set_number"] = 1
+        st.success(f"Exercise added! ID: {workout_exercises_id}")
+        st.session_state["workout_exercises_id"] = workout_exercises_id
 
 # UI to insert the sets, reps & weights into the table "exercises_sets"
 
@@ -73,11 +104,21 @@ from db import create_exercises_sets
 st.subheader("Add Sets")
 
 set_number = st.session_state["set_number"]
-st.write(f"Set Number: {set_number}")
+st.write(f"Set Number: {st.session_state['set_number']}")
 reps = st.number_input("Reps", min_value=1, step=1)
 weight = st.number_input("Weight (kg)", min_value=0.0, step=2.5)
 duration_seconds = st.number_input("Duration (secs)", min_value = 5, step = 1)
 
+# Initialize message state
+message_placeholder =st.empty()
+
+if "success_message" not in st.session_state:
+    st.session_state["success_message"] = None
+
+if st.session_state["success_message"]:
+    message_placeholder.success(st.session_state["success_message"])
+    st.session_state["success_message"] = None
+    
 if st.button("Add Set"):
     workout_exercises_id = st.session_state.get("workout_exercises_id")
 
@@ -92,13 +133,31 @@ if st.button("Add Set"):
             duration_seconds
         )
 
-        st.success(f"Set added! ID: {set_id}")
+        st.session_state["success_message"] = f"Set {st.session_state['set_number']} added!"
         st.session_state["set_number"] += 1
-
-from db import get_exercises_for_workout
-exercises_for_display = get_exercises_for_workout(st.session_state["workout_session_id"])
+        st.rerun()
 
 st.subheader("Workout Progress")
 
-for ex in exercises_for_display:
-    st.write(ex)
+from db import get_whole_workout_session
+
+from collections import defaultdict
+
+workout_session_id = st.session_state.get("workout_session_id")
+
+if workout_session_id:
+    rows = get_whole_workout_session(workout_session_id)
+    grouped = defaultdict(list)
+
+    for row in rows:
+        exercise_key = (row[0], row[1], row[2], row[3])  
+        grouped[exercise_key].append(row)
+    
+    for (we_id, name, muscle, order), sets in grouped.items():
+
+        st.markdown(f"### {order}. {name} ({muscle})")
+
+        for s in sets:
+            st.write(
+                f"Set {s[4]} | Reps: {s[5]} | Weight: {s[6]}"
+        )
